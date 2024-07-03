abd37f239f5c00269630afe6307c7f5185ee8071
Billard Gautier <gautier.billard@mappy.com>
2024-07-03 08:59:37 +0200

    Setup project.

---
+++ Todoliti/TodolitiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodolitiApp.swift
+//  Todoliti
+//
+//  Created by Gautier BILLARD on 03/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct TodolitiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
